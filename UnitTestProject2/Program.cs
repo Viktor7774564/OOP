db857faa2a87b3e7e14a5bbdbf3678fbf4609ad9
310-09 <310-09@310-09>
2021-12-08 10:00:45 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+class Programmer 
+{
+    public string Fam;
+    public int numOfProg;
+    public int numOfLang;
+    public int Q;
+
+    public int Quality1()
+    {
+        Q = numOfProg * numOfLang;
+        return Q;
+    }
+}
+
+class Rasch 
+{
+    Programmer p1 = new Programmer();
+    int P;
+    int Qp;
+    public int Quality2(int Q, int nFp) 
+    {
+        bool Prov = false;
+        Console.WriteLine("Введите число программ программиста, которые работают правильно:");
+        while (Prov == false) 
+        {
+            P = Convert.ToInt32(Console.ReadLine());
+            if (P <= nFp) 
+            { 
+             Prov = true;
+            }
+            else 
+            { 
+             Prov=false;
+            }
+        }
+        
+            
+        Qp = Q * P;
+        return Qp;
+    }
+}
+
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool res = false;
+            Programmer p = new Programmer();
+            Rasch rasch = new Rasch();
+            Console.WriteLine("Введите фамилию программиста:");
+            p.Fam = Console.ReadLine();
+            Console.WriteLine("Введите число программ, написанных программистом:");
+            p.numOfProg = Convert.ToInt32(Console.ReadLine()); //p.numOfProg=10
+            Console.WriteLine("Введите число языков, которыми он пишет программы:");
+            p.numOfLang = Convert.ToInt32(Console.ReadLine()); //p.numOfLang = 10
+            int Q1 = p.Quality1();
+            int Qp1 = rasch.Quality2(Q1, p.numOfProg);
+            int Q2 = 100;
+
+            res = ReferenceEquals(Q1, Q2);
+        }
+    }
+}
